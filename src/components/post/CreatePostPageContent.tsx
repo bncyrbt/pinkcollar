ce3d5630e3f0a3463f87e4f1ff15239175d9265f
bouncy-rabbit <caroyarin@gmail.com>
2025-05-18 20:21:00 -0400

    header for post page

--- src/components/post/CreatePostPageContent.tsx
+++ src/components/post/CreatePostPageContent.tsx
@@ -1,20 +1,35 @@
 import { AddContributorsBlock } from "../contribution/AddContributorsBlock";
 import { PageContentLayout } from "../layout/PageContentLayout";
+import { Divider } from "../ui/divider";
 import { PostInfoBlock } from "./PostInfoBlock";
 
 export const CreatePostPageContent = () => {
   return (
-    <PageContentLayout
-      main={
-        <>
-          <PostInfoBlock />
-        </>
-      }
-      aside={
-        <div>
-          <AddContributorsBlock />
-        </div>
-      }
-    />
+    <div className="flex flex-col w-full h-full">
+      <CreatePostHeader />
+      <Divider />
+      <PageContentLayout
+        main={
+          <>
+            <PostInfoBlock />
+          </>
+        }
+        aside={
+          <div>
+            <AddContributorsBlock />
+          </div>
+        }
+      />
+    </div>
+  );
+};
+
+const CreatePostHeader = () => {
+  return (
+    <div className="pl-16 py-4 flex flex-row items-center gap-4">
+      <div className="text-2xl">
+        <span className="font-bold">Create Post</span>
+      </div>
+    </div>
   );
 };
--- src/components/post/PostInfoBlock.tsx
+++ src/components/post/PostInfoBlock.tsx
@@ -1,11 +1,20 @@
 import { Block } from "../layout/Block";
+import { ProfileAvatar } from "../profile/ProfileAvatar";
 import { PostInfoContent } from "./PostInfoContent";
 
 export const PostInfoBlock = () => {
   return (
     <Block
       className="border-b border-black"
-      header={<div className="font-bold">Post Info </div>}
+      header={
+        <div className="pl-16 py-4 flex flex-row items-center gap-4">
+          <ProfileAvatar variant="small" />
+          <div className="text-xl">
+            <span className="font-bold">Tomer Even Ari</span>
+            <span>/Suzi Blazer</span>
+          </div>
+        </div>
+      }
       main={<PostInfoContent />}
     />
   );
